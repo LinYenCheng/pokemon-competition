4d36ebb016302d8312b510cbe39512bd10b51424
LinYenCheng <breaking0215@hotmail.com>
2022-12-02 00:46:37 +0800

    chore: update package

--- src/index.js
+++ src/index.js
@@ -1,41 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
-
-// dynamic User by Hux
-var _gaId = "UA-106834789-1";
-var _gaDomain = "linyencheng.github.io";
-
-// if (location.host === _gaDomain) {
-//   // Originial
-//   (function(i, s, o, g, r, a, m) {
-//     i["GoogleAnalyticsObject"] = r;
-//     (i[r] =
-//       i[r] ||
-//       function() {
-//         (i[r].q = i[r].q || []).push(arguments);
-//       }),
-//       (i[r].l = 1 * new Date());
-//     (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-//     a.async = 1;
-//     a.src = g;
-//     m.parentNode.insertBefore(a, m);
-//   })(
-//     window,
-//     document,
-//     "script",
-//     "//www.google-analytics.com/analytics.js",
-//     "ga"
-//   );
-
-//   ga("create", _gaId, _gaDomain);
-//   ga("send", "pageview");
-// }
